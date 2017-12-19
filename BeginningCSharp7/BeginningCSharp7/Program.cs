3b53f62b01655422d09165c223f744c3458f898f
Benjamin Perkins <live@perkinsville.com>
2017-12-19 14:24:40 +0100

    Beginning C# 7 Programming with Visual Studio 2017

---
+++ BeginningCSharp7/BeginningCSharp7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace BeginningCSharp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Welcome to Beginning C# 7 Programming with Visual Studio 2017.  Press the ENTER key to exit.");
+            ReadLine();
+        }
+    }
+}
