ed809926b51edad268197b2d957edc6ea8d59d39
Benjamin Perkins <live@perkinsville.com>
2017-12-19 14:33:43 +0100

    Chapter 2: Writing a C# Program

---
+++ Chapter02/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Output text to the screen.
+            Console.WriteLine("The first app in Beginning Visual C# 2017!");
+            Console.ReadKey();
+        }
+    }
+}
