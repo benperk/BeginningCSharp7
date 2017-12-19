45d790a6f8dcaa3eb56433454e985c453fd7df5d
Benjamin Perkins <live@perkinsville.com>
2017-12-19 14:38:18 +0100

    Chapter 3: Variables and Expressions

---
+++ Chapter03/Ch03Ex01/Ch03Ex01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch03Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInteger;
+            string myString;
+            myInteger = 17;
+            myString = "\"myInteger\" is";
+            Console.WriteLine($"{myString} {myInteger}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter03/Ch03Ex02/Ch03Ex02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch03Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber, secondNumber;
+            string userName;
+            Console.WriteLine("Enter your name:");
+            userName = Console.ReadLine();
+            Console.WriteLine($"Welcome {userName}!");
+            Console.WriteLine("Now give me a number:");
+            firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Now give me another number:");
+            secondNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is " +
+                          $"{firstNumber + secondNumber}.");
+            Console.WriteLine($"The result of subtracting {secondNumber} from " +
+                          $"{firstNumber} is {firstNumber - secondNumber}.");
+            Console.WriteLine($"The product of {firstNumber} and {secondNumber} " +
+                          $"is {firstNumber * secondNumber}.");
+            Console.WriteLine($"The result of dividing {firstNumber} by " +
+                          $"{secondNumber} is {firstNumber / secondNumber}.");
+            Console.WriteLine($"The remainder after dividing {firstNumber} by " +
+                          $"{secondNumber} is {firstNumber % secondNumber}.");
+            Console.ReadKey();
+
+        }
+    }
+}
